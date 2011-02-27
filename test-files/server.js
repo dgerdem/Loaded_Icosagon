f67ceca191234d9adad50470d6c926aeaee275a2
David Gerdeman <dave.gerdeman@gmail.com>
2011-02-27 16:17:45 -0600

    added additional parsing code to server

--- test-files/server.js
+++ test-files/server.js
@@ -1,6 +1,7 @@
 var net = require('net');
 var num = 1;
-comms = [];
+var comms = [];  //contains connection data for clients
+var players = [];  //contains IDs of clients that have connected
 
 var server = net.createServer(function (c) {
 	c.write('hello\r\n', encoding='ascii');
@@ -10,20 +11,28 @@ var server = net.createServer(function (c) {
 
 	c.on('data', function(hand) {
 		var str = hand.toString('ascii', start=0, end=hand.length);
-		console.log(typeof str);
 		var parts = str.split(',');
-		console.log(parts);
 		if (parts[0] == 'narrow') {
-			console.log("Narrowcasting");
 			comms[parts[1]].write('"' + parts[2] + '"' + ' was sent to you from ' +
 				c.remoteAddress + ':' + c.remotePort + '\r\n', encoding='ascii');
 		} else if (parts[0] == 'broad') {
-			console.log("Broadcasting");
 			for (con in comms) {
 				comms[con].write(parts[1] + " was recieved and sent to you\r\n", encoding='ascii');	
 			}
+		} else if (parts[0] == 'id') { 
+		//event must be recieved after a connection is made...
+			players.push(parts[1]);
+			for (con in comms) {
+				comms[con].write("players," + arrayToString(), encoding='ascii');	
+			}
 		}
 	});
+	
+	function arrayToString() {
+		var temp_str = players.toString();
+		var swapped = temp_str.replace(/,/gi, ';');
+		return swapped;
+	}
 });
 
 server.listen(8124, 'localhost');
