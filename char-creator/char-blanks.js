603b1e749bf50565b7b829286bf63ef77345e45e
David Gerdeman <dave.gerdeman@gmail.com>
2011-04-26 21:12:03 -0500

    added a few more fields I forgot

---
+++ char-creator/char-blanks.js
@@ -0,0 +1,33 @@
+Blank_sheet = 
+{
+	"char_name":0,"race":0,"align":0,"cclass":0,
+	"level":0,"tot_xp":0,"xp_ch":0,"nex_lvl_xp":0,
+	"diety":0,"size":0,"age":0,"gender":0,
+	"height":0,"weight":0,"eyes":0,"hair":0,
+	
+	"str":0,"str_mod":0,"temp_str":0,
+	"dex":0,"dex_mod":0,"temp_dex":0,
+	"con":0,"con_mod":0,"temp_con":0,
+	"int":0,"int_mod":0,"temp_int":0,
+	"wis":0,"wis_mod":0,"temp_wis":0,
+	"cha":0,"cha_mod":0,"temp_cha":0,
+	
+	"tot_hp":0,"cur_hp":0,"non_leth":0,"hit_d":0,"dmg_cut":0,
+	"tot_ac":0,"armor":0,"shield":0,"misc_ac":0,"deflect_ac":0,"nat_ac":0,
+	"touch":0,"flat_foot":0,
+	"init":0,"misc_init":0,"speed":0,"armor_type":0,
+	
+	"fort_tot":0,"base_fort":0,"misc_fort":0,"temp_fort":0,
+	"reflex_tot":0,"base_reflex":0,"misc_reflex":0,"temp_reflex":0,
+	"will_tot":0,"base_will":0,"misc_will":0,"temp_will":0,
+	
+	"mel_tab":0,"mel_bab":0,"size_mod":0,"mel_misc":0,"mel_temp":0,
+	"ran_tab":0,"ran_bab":0,"ran_misc":0,"ran_temp":0,
+
+	"ap":0
+	
+};
+Blank_inventory = {};
+Blank_spells = {};
+Blank_skills = {};
+Blank_feats = {};
