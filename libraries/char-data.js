bf86592083cbccc56d734b1fdb244fad50cb89c9
David Gerdeman <dave.gerdeman@gmail.com>
2011-04-26 21:14:04 -0500

    will now need to be passed the name of the local storage object to open, save, or check for.

--- libraries/char-data.js
+++ libraries/char-data.js
@@ -1,22 +1,22 @@
 Data = {
 	sheet:0 ,
 	
-	init: function() {
+	init: function(store_name) {
 //check if the localstorages exist, call load if it does, otherwise, prompt for input?
-		if (localStorage.getItem("char-data")){
+		if (localStorage.getItem(store_name)){
 			return true;
 		} else {
 			return false;
 		}
 	},
 //loads the data stores...probably should return a json object to pass around...	
-	load: function() {
-		var data = localStorage.getItem("char-data");
+	load: function(store_name) {
+		var data = localStorage.getItem(store_name);
 		var jsonData = JSON.parse(data);
 		return jsonData;
 	},
 //saves the data store to the local storage	
-	save: function(dat) {
-		localStorage.setItem("char-data", dat);
+	save: function(store_name, data) {
+		localStorage.setItem(store_name, data);
 	}
 };
