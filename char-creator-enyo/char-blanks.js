0683148840e326c0e04b8d8c6cef0cd6bc1994b4
David Gerdeman <dave.gerdeman@gmail.com>
2011-08-02 20:57:24 -0500

    minor changes

---
+++ char-creator-enyo/char-blanks.js
@@ -0,0 +1,31 @@
+
+Blank_inventory = {id:[],quants:[],descr:[],mods:[],rolls:[],equiped:[]};
+Blank_spells = {id:[],lvl:[],castime:[],dur:[],targets:[],range:[],save:[],resist:[]};
+Blank_skills = {id:[],ablity:[],points:[]};
+Blank_feats = {id:[],descr:[],rolls:[]};
+Blank_Sheet = {
+		char_name:"",race:"",align:"",cclass:"",
+		level:"",tot_xp:"",xp_ch:"",nex_lvl_xp:"",
+		diety:"",size:"",age:"",gender:"",
+		height:"",weight:"",eyes:"",hair:"",
+	
+		str:"",str_mod:"",temp_str:"",
+		dex:"",dex_mod:"",temp_dex:"",
+		con:"",con_mod:"",temp_con:"",
+		int1:"",int_mod:"",temp_int:"",
+		wis:"",wis_mod:"",temp_wis:"",
+		cha:"",cha_mod:"",temp_cha:"",
+	
+		tot_hp:"",cur_hp:"",non_leth:"",hit_d:"",dmg_cut:"",
+		tot_ac:"",armor:"",shield:"",misc_ac:"",
+		touch:"",flat_foot:"",
+		init:"",misc_init:"",speed:"",armor_type:"",
+	
+		fort_tot:"",base_fort:"",misc_fort:"",temp_fort:"",
+		reflex_tot:"",base_reflex:"",misc_reflex:"",temp_reflex:"",
+		will_tot:"",base_will:"",misc_will:"",temp_will:"",
+	
+		mel_tab:"",mel_bab:"",size_mod:"",mel_misc:"",mel_temp:"",
+		ran_tab:"",ran_bab:"",ran_misc:"",ran_temp:"",
+
+		ap:""};
